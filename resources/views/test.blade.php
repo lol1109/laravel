30741a424dfc3cf7269714cc228d8097dde8a5df
lol1109 <gamingkhaizer@gmail.com>
2023-10-17 10:58:46 +0700

    menambahkan kata 'tidak ada pengunjung' jika tidak ada data pada bulan atau tahun

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,13 +4,13 @@
     <div class="container">
         <div class="card mt-3">
             <div class="card-header">
-            	<div class="col-12">
-            		 <h3 class="card-title"> Data Pengunjung</h3>
-            	</div>
+                <div class="col-12">
+                    <h3 class="card-title"> Data Pengunjung</h3>
+                </div>
             </div>
             <div class="card-body">
-            		<form action="{{ route('search') }}" method="GET">
-            		<div class="row">
+                <form action="{{ route('search') }}" method="GET">
+                    <div class="row">
             		<select class="form-select" style="width: 10%; margin-right: 1rem; margin-left: 1rem;" name="month">
     					<option value="01">Januari</option>
     					<option value="02">Februari</option>
@@ -37,52 +37,56 @@
 					</select>
 					<button class="btn btn-success" style="width: 10%;" type="submit">Filter</button>
 					</div>
-        		</form>
-            	<!-- Form untuk memilih bulan dan tahun -->
-            	@if($error == True)
-            		<h3 class="text-center mt-5">Form Tidak Boleh Diedit</h3>
-            	@else
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Target</th>
-                            <th>Traffic</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php 
-                        	$no = 1; 
-                        	$displayedCombinations = []; 
-                        @endphp	
-						@foreach($visits as $visit)
-    						@php
-    							$target = $visit->target;
-    							$trafic = $visit->traffict;
-    						@endphp
-    						@if (!in_array($target . ' ' . $trafic, $displayedCombinations))
-        				<tr>
-            				<td>{{ $no++ }}</td>
-            				<td>{{ $target }}</td>
-            				<td>{{ $trafic }}</td>
-            				<td>
-            					@if($target == 'epi' && $trafic == 'instagram')
-            						 {{ $totalEpiIg }}
-            					@elseif($target == 'epi' && $trafic == 'googleads')
-                					 {{ $totalEpiGa }}
-            					@elseif($target == 'emot' && $trafic == 'instagram')
-                					{{ $totalEmotIg }}
-            					@elseif($target == 'emot' && $trafic == 'googleads')
-               	 					{{ $totalEmotGa }}
-            					@endif
-        					</td>
-        				</tr>
-        					<?php $displayedCombinations[] = $target . ' ' . $trafic; ?>
-    						@endif
-						@endforeach
-                    </tbody>
-                </table>
+                </form>
+                <!-- Form untuk memilih bulan dan tahun -->
+                @if ($error == True)
+                    <h3 class="text-center mt-5">Form Tidak Boleh Diedit</h3>
+                @else
+                    @if (count($visits) > 0)
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Target</th>
+                                    <th>Traffic</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $no = 1;
+                                    $displayedCombinations = [];
+                                @endphp
+                                @foreach ($visits as $visit)
+                                    @php
+                                        $target = $visit->target;
+                                        $trafic = $visit->traffict;
+                                    @endphp
+                                    @if (!in_array($target . ' ' . $trafic, $displayedCombinations))
+                                        <tr>
+                                            <td>{{ $no++ }}</td>
+                                            <td>{{ $target }}</td>
+                                            <td>{{ $trafic }}</td>
+                                            <td>
+                                                @if ($target == 'epi' && $trafic == 'instagram')
+                                                    {{ $totalEpiIg }}
+                                                @elseif ($target == 'epi' && $trafic == 'googleads')
+                                                    {{ $totalEpiGa }}
+                                                @elseif ($target == 'emot' && $trafic == 'instagram')
+                                                    {{ $totalEmotIg }}
+                                                @elseif ($target == 'emot' && $trafic == 'googleads')
+                                                    {{ $totalEmotGa }}
+                                                @endif
+                                            </td>
+                                        </tr>
+                                        <?php $displayedCombinations[] = $target . ' ' . $trafic; ?>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <h3 class="text-center mt-5">Tidak Ada Pengunjung</h3>
+                    @endif
                 @endif
             </div>
         </div>
